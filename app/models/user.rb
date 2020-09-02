d453cf6cf55948201e8803deb9559d95873e3721
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-09-02 20:40:23 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
+  devise :validatable, password_length: 7..128
   validates :nickname, presence: true, length: { maximum: 50 }
   validates :username, presence: true, length: { maximum: 15 }, uniqueness: true, format: { with: /\A@[a-z0-9]+\z/ }
   validates :email, presence: true, uniqueness: true, format: { with: /\A\S+@\S+\.\S+\z/}
-  validates :password, presence: true, length: { minimum: 7 }, format: { with: /\A[a-z0-9]+\z/i }
+  validates :password, presence: true, format: { with: /\A[a-z0-9]+\z/i }
 end

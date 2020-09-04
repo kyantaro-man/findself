58615633fef2c84f87bc2b86a6d45a03ac5434e9
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-09-05 08:06:30 +0900

    Update profile.rb

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,4 +1,5 @@
 class Profile < ApplicationRecord
+  validates :catch_copy, length: { maximum: 100 }
   validates :introduction, length: { maximum: 3000 }
   validates :goal, length: { maximum: 300 }
   validates :career, length: { maximum: 2000 }

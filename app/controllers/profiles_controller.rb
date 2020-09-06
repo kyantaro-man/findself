f6b4e79d6e435641f74ba73165730422449029ad
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-09-06 12:16:24 +0900

    Update Basic show for profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -9,18 +9,25 @@ class ProfilesController < ApplicationController
   def edit
     @user = User.find(current_user.id)
     @profile = @user.profile
+    @basic = @user.basic
   end
 
   def update
     @user = User.find(current_user.id)
     @profile = @user.profile
+    @basic = @user.basic
     @profile.update(profile_params)
+    @basic.update(basic_params)
   end
 
   private
 
   def profile_params
     params.required(:profile).permit(:cover_image, :profile_image, :catch_copy, :introduction, :goal, :career, :related_title, :related_link, :attached_file)
+  end
+
+  def basic_params
+    params.required(:basic).permit(:birth_place, :birth_date, :blood_type, :gender)
   end
 
   def set_user

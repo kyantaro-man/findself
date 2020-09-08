106b3f9f9f7c684f0d4579242c00c0f2ba095913
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-09-08 13:59:00 +0900

    Show&Hide markdown

---
+++ app/javascript/jquery/src/profiles_markdown.js
@@ -0,0 +1,25 @@
+$(function() {
+  $(document).on("click", '.js-edit-markdown-button', function() {
+    let markdownId = $(this).data('target');
+    let markdownLabelArea = $('#js-label-' + markdownId);
+    let markdownTextArea = $('#js-textarea-' + markdownId);
+    let markdownButton = $('#js-button-' + markdownId);
+
+    markdownLabelArea.hide();
+    markdownTextArea.show();
+    markdownButton.show();
+  })
+})
+
+$(function() {
+  $(document).on("click", '.markdown-cancel-button', function() {
+    let markdownId = $(this).data('cancel-id');
+    let markdownLabelArea = $('#js-label-' + markdownId);
+    let markdownTextArea = $('#js-textarea-' + markdownId);
+    let markdownButton = $('#js-button-' + markdownId);
+
+    markdownLabelArea.show();
+    markdownTextArea.hide();
+    markdownButton.hide();
+  })
+})
